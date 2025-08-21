d2a1185dde55d4f5c77bc8778fc893c6ee22b24c
Daniel Paixão <daniel_dpx@icloud.com>
2025-08-21 03:23:44 -0300

    fix: correção tabela system default_language
    alterado arquivo language_portuguese_2.0.0.sql inserindo valores das colunas group_title e sub_group.
    
    resolvendo falha no menu lateral em /systems/configuration onde o item do sidemenu não exibia o nome do Menu.

--- web_interface/flux/addons/plugins/language_portuguese/database/language_portuguese_2.0.0.sql
+++ web_interface/flux/addons/plugins/language_portuguese/database/language_portuguese_2.0.0.sql
@@ -1,3 +1,3 @@
 INSERT INTO `languages` ( `code`, `name`, `locale`) VALUES ('pt', 'Portuguese', 'pt_BR');
 ALTER TABLE `translations` ADD `pt_BR` TEXT NOT NULL;
-INSERT INTO `system` ( `name`, `value`, `field_type`, `reseller_id`, `is_display`) VALUES ('default_language', 'Portuguese', 'default_system_input', '0', '1');
+INSERT INTO `system` (`name`, `display_name`, `value`, `field_type`, `reseller_id`, `is_display`, `group_title`, `sub_group`) VALUES ('default_language', '', 'Portuguese', 'default_system_input', '0', '1', 'global', 'General');
